2aa30dbda63ba4d10172e76f0b160fa5028ccf63
Dmytro Davydov <dmytro@crowdkeep.com>
2025-04-18 11:03:09 +0100

    @nestjs/config added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { FirebaseModule } from './firebase/firebase.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, FirebaseModule],
+  imports: [UsersModule, FirebaseModule, ConfigModule.forRoot({isGlobal: true})],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  // example accessing a config value
+  constructor(private readonly configService: ConfigService) {
+    const dbName = this.configService.get<string>('DB_NAME');
+    console.log('Database Host:', dbName);
+  }
   getHello(): string {
+    console.log
     return 'Hello World!';
   }
 }
